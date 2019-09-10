0dd68e874d965c71eb0c5b8bd1c0ef23e54c8f0f
Luc Didry <luc@didry.org>
2019-09-10 12:46:11 +0200

    Use new NPM package name

--- eejs.js
+++ eejs.js
@@ -2,6 +2,6 @@ var eejs = require('ep_etherpad-lite/node/eejs/');
 
 exports.eejsBlock_mySettings = function (hook_name, args, cb)
 {
-  args.content = args.content + eejs.require('ep_user_font_size/templates/user_font_sizeSettings.ejs', {settings : false});
+  args.content = args.content + eejs.require('ep_user_fontsize_version_2/templates/user_font_sizeSettings.ejs', {settings : false});
   return cb();
 }

1c6abe745c487b78b7424beed1282af027ce2683
John McLear <john@mclear.co.uk>
2020-11-22 18:22:28 +0000

    autofixes from Etherpad checkPlugins.js

--- eejs.js
+++ eejs.js
@@ -1,7 +1,6 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
+const eejs = require('ep_etherpad-lite/node/eejs/');
 
-exports.eejsBlock_mySettings = function (hook_name, args, cb)
-{
-  args.content = args.content + eejs.require('ep_user_fontsize_version_2/templates/user_font_sizeSettings.ejs', {settings : false});
+exports.eejsBlock_mySettings = function (hook_name, args, cb) {
+  args.content += eejs.require('ep_user_fontsize_version_2/templates/user_font_sizeSettings.ejs', {settings: false});
   return cb();
-}
+};
--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,13 @@
-exports.postAceInit = function(name, context){
-  var padOuter = $('iframe[name="ace_outer"]').contents();
-  var padInner = padOuter.find('iframe[name="ace_inner"]');
+exports.postAceInit = function (name, context) {
+  const padOuter = $('iframe[name="ace_outer"]').contents();
+  const padInner = padOuter.find('iframe[name="ace_inner"]');
 
-  $('#user-font-size').change(function(){
-    var newVal = 20 + parseInt($(this).val()) +"%";
+  $('#user-font-size').change(function () {
+    const newVal = `${20 + parseInt($(this).val())}%`;
 
     // line-height behaves weird, can't really solve it easily.
     // For now this bodge fix will do
-    padInner.contents().find("body").css("line-height", newVal);
-    padInner.contents().find("body").css("font-size", newVal);
+    padInner.contents().find('body').css('line-height', newVal);
+    padInner.contents().find('body').css('font-size', newVal);
   });
-}
-
+};

60c96d74ed084f52f83dba35e8f3ecdd9f28dd7b
Sebastian Sproesser <elzoido@elbloggo.de>
2021-01-19 12:53:13 +0100

    Toolbar, not Settings

--- eejs.js
+++ eejs.js
@@ -1,6 +1,6 @@
 const eejs = require('ep_etherpad-lite/node/eejs/');
 
-exports.eejsBlock_mySettings = function (hook_name, args, cb) {
-  args.content += eejs.require('ep_user_fontsize_version_2/templates/user_font_sizeSettings.ejs', {settings: false});
+exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
+  args.content += eejs.require('ep_user_font_size/templates/user_font_sizeToolbar.ejs', {settings: false});
   return cb();
 };
--- static/js/index.js
+++ static/js/index.js
@@ -10,4 +10,13 @@ exports.postAceInit = function (name, context) {
     padInner.contents().find('body').css('line-height', newVal);
     padInner.contents().find('body').css('font-size', newVal);
   });
+  
+  $('#user-font-size-toolbar').change(function () {
+    const newVal = `${20 + parseInt($(this).val())}%`;
+
+    // line-height behaves weird, can't really solve it easily.
+    // For now this bodge fix will do
+    padInner.contents().find('body').css('line-height', newVal);
+    padInner.contents().find('body').css('font-size', newVal);
+  });
 };

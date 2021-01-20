f3aca1433562ca66137802868ff444d6cad3efe7
Sebastian Sproesser <elzoido@elbloggo.de>
2021-01-20 09:37:48 +0100

    Show in right toolbar

--- eejs.js
+++ eejs.js
@@ -1,6 +1,6 @@
 const eejs = require('ep_etherpad-lite/node/eejs/');
 
-exports.eejsBlock_editbarMenuLeft = function (hook_name, args, cb) {
+exports.eejsBlock_editbarMenuRight = function (hook_name, args, cb) {
   args.content += eejs.require('ep_user_font_size/templates/user_font_sizeToolbar.ejs', {settings: false});
   return cb();
 };
